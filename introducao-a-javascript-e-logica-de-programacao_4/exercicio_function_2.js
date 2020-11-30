8def453ed22c99fd6b8623d8ecb5dcb789cf2b0d
Bruno <brunocmoraes97@gmail.com>
2020-11-30 19:56:17 -0300

    x

---
+++ introducao-a-javascript-e-logica-de-programacao_4/exercicio_function_2.js
@@ -0,0 +1,9 @@
+/*function biggerIndex(array){
+    array = array.sort
+    let index = array.indexof(array.length)
+    console.log(array);
+}*/
+
+let array = [2, 3, 6, 7, 10, 1]
+array.sort();
+console.log(array);
