1102a0022500d6d042388918cee8732b46fd3c85
Bruno <brunocmoraes97@gmail.com>
2020-12-03 18:58:56 -0300

    Compara valores, e retornos de uma função

--- introducao-a-javascript-es6-e-testes-unitarios_3/soma.js
+++ introducao-a-javascript-es6-e-testes-unitarios_3/soma.js
@@ -9,10 +9,10 @@ function sum(a, b) {
 };
 
 //Teste se o retorno de sum(4,5) é 9.
-assert.equal(sum(4, 5), 9);
+assert.equal(sum(4, 5), 9, '4 + 5 é igual a 9');
 
 //Teste se o retorno de sum(0, 0) é 0.
-assert.equal(sum(0, 0), 0);
+assert.equal(sum(0, 0), 0, '0 + 0 é igual a 0');
 
 //Teste se a função sum lança um erro quando os parametros são 4 e "5" (sendo 5 uma string).
 assert.strictEqual(sum(4, '5'), 9, '5 não é um número');
