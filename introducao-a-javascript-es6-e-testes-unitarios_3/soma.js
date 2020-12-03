d191644e52b8c3fca6090fec761c76c2737d73da
Bruno <brunocmoraes97@gmail.com>
2020-12-03 17:17:32 -0300

    Adiciona exercícios de testes de soma e verificação se parametro é string

---
+++ introducao-a-javascript-es6-e-testes-unitarios_3/soma.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+};
+
+//Teste se o retorno de sum(4,5) é 9.
+assert.equal(sum(4, 5), 9);
+
+//Teste se o retorno de sum(0, 0) é 0.
+assert.equal(sum(0, 0), 0);
+
+//Teste se a função sum lança um erro quando os parametros são 4 e "5" (sendo 5 uma string).
+assert.strictEqual(sum(4, '5'), 9, '5 não é um número');
