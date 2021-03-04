de4b95b0cb1a704106c94dda7fc4964078ba1118
Bruno <brunocmoraes97@gmail.com>
2021-03-04 12:54:24 -0300

    Exercicio para praticar React Router

---
+++ extra-exercises/ReactRouter.1/react-router/src/Data.js
@@ -0,0 +1,7 @@
+const aulas = [
+    { id: 0, title: 'Aula inicial', desc: 'Momento inicial e apresentações' },
+    { id: 1, title: 'Como configurar', desc: 'Configurações do Router'},
+    { id: 2, title: 'Hello World', desc: 'Fazendo o primeiro Hello World'}
+]
+
+export default aulas;
---
+++ extra-exercises/ReactRouter.1/react-router/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../App.css'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className='page'>
+        <h1>Olá, essa é a Página Principal!</h1>
+      </div>
+    );
+  }
+}
