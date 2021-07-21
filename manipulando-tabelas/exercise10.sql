82b4aada09ddf457f61c294f08fbee605d69752b
Bruno <brunocmoraes97@gmail.com>
2021-07-21 15:54:22 -0300

    Adiciona exercícios do 4 ao 10. Aula de manipulação de tabelas

---
+++ manipulando-tabelas/exercise10.sql
@@ -0,0 +1,9 @@
+SELECT actor_id
+FROM sakila.actor
+WHERE first_name = 'MATTHEW';
+
+DELETE FROM sakila.film_actor
+WHERE actor_id IN (8, 103, 181);
+
+DELETE FROM sakila.actor
+WHERE first_name = 'MATTHEW'
---
+++ manipulando-tabelas/exercise9.sql
@@ -0,0 +1,5 @@
+DELETE FROM sakila.film_actor
+WHERE actor_id = 12;
+
+DELETE FROM sakila.actor
+WHERE first_name = 'KARL';
