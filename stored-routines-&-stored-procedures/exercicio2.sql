34973a4e500aeadd158772efbb9f3f4ab4238fc0
Bruno <brunocmoraes97@gmail.com>
2021-07-29 11:49:46 -0300

    Exercício 2

---
+++ stored-routines-&-stored-procedures/exercicio2.sql
@@ -0,0 +1,20 @@
+USE sakila;
+DELIMITER $$
+
+CREATE PROCEDURE FindMovieByCategory(IN category VARCHAR(100))
+BEGIN
+    SELECT f.film_id, f.title, fc.category_id, c.name
+    FROM sakila.film f
+    INNER JOIN sakila.film_category fc ON f.film_id = fc.film_id
+    INNER JOIN sakila.category c ON c.category_id = fc.category_id
+    WHERE c.name = category;
+END $$
+
+DELIMITER ;
+
+
+CALL FindMovieByCategory('Action');
+
+
+
+   
