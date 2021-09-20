8151842f2bab109bd31310f48b4063849ef84c83
Bruno <brunocmoraes97@gmail.com>
2021-09-20 18:17:17 -0300

    Adiciona perguntas dinâmicas atráves do módulo readline

--- hello-word/index.js
+++ hello-word/index.js
@@ -1,1 +1,7 @@
-console.log('Hello, world');
+const readline = require('readline-sync');
+
+const name = readline.question('Qual o seu nome?: ');
+const age = readline.questionInt('Qual a sua idade ?: ');
+
+
+console.log(`Seu nome é ${name} e sua idade é ${age}`);
