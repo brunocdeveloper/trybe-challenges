5195f80e807825c0d7d556cf6766eb8953008585
Bruno <brunocmoraes97@gmail.com>
2021-09-23 16:33:41 -0300

    Corrige os exercicios que estavam faltando o it como asserçoes

---
+++ testes-em-node-parte-2/test.js
@@ -0,0 +1,51 @@
+const { expect } = require('chai');
+const avaliaNumero = require('./avaliaNumero');
+
+describe('Testa a função avaliaNumero', () => {
+  describe('Quando número for maior que 0', () => {
+    describe('A resposta é', () => {
+      it('é uma string', () => {
+        const resposta = avaliaNumero(2);
+        expect(resposta).to.be.a('string');
+      });
+
+      it('é igual a "positivo"', () => {
+        const resposta = avaliaNumero(2);
+        expect(resposta).to.be.equals('positivo');
+      });
+    });
+  });
+});
+
+describe('Testa a função avaliaNumero', () => {
+  describe('Quando número for menor que 0', () => {
+    describe('A resposta é', () => {
+      it('é uma string', () => {
+        const resposta = avaliaNumero(-1);
+        expect(resposta).to.be.a('string');
+      });
+
+
+      it('é igual a "negativo"', () => {
+        const resposta = avaliaNumero(-1);
+        expect(resposta).to.be.equals('negativo');
+      });
+    });
+  });
+});
+
+describe('Testa a função avaliaNumero', () => {
+  describe('Quando número for igual a 0', () => {
+    describe('A resposta é', () => {
+      it('é uma string', () => {
+        const resposta = avaliaNumero(0);
+        expect(resposta).to.be.a('string');
+      });
+
+      it('é igual a "neutro"', () => {
+        const resposta = avaliaNumero(0);
+        expect(resposta).to.be.equals('neutro');
+      });
+    });
+  });
+});
