b116249b26944d117477e59d1a41b0dc4a60ae27
Bruno <brunocmoraes97@gmail.com>
2021-09-23 12:38:58 -0300

    Realiza os testes manuais para a função do exercício 1

--- testes-em-nodejs/chamadaexercicio1.js
+++ testes-em-nodejs/chamadaexercicio1.js
@@ -1,3 +1,27 @@
 const aprovacao = require('./exercicio1');
 
-console.log(aprovacao(4));
+const media = aprovacao(4);
+
+console.log('Quando a média for menor que 7 retorna "Reprovado"');
+
+if(media == 'Aprovado') {
+  console.log('Ok');
+} else {
+  console.error('Resposta não esperada');
+};
+
+console.log('Quando a média for maior que 7 retorna "Aprovado"');
+
+if(media == 'Aprovado') {
+  console.log('Ok');
+} else {
+  console.error('Resposta não esperada');
+};
+
+console.log('Quando a média for igual a 7 retorna "aprovado"');
+
+if(media == 'Aprovado') {
+  console.log('Ok');
+} else {
+  console.error('Resposta não esperada');
+};
