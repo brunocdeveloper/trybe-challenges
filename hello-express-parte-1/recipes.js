70672174dd46f062813f305fb769013d3c17965e
Bruno <brunocmoraes97@gmail.com>
2021-09-24 16:48:41 -0300

    Adiciona funcionalidade de pesquisar por query string

--- hello-express-parte-1/recipes.js
+++ hello-express-parte-1/recipes.js
@@ -8,13 +8,19 @@ const recipes = [
 ];
 
 
-app.get('/recipes/:id', function (req, res) {
+/* app.get('/recipes/:id', function (req, res) {
   const { id } = req.params;
   const recipe = recipes.find((r) => r.id === parseInt(id));
 
   if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
 
   res.status(200).json(recipe);
+}); */
+
+app.get('/recipes/search', function (req, res) {
+  const { name, maxPrice } = req.query;
+  const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.price < parseInt(maxPrice));
+  res.status(200).json(filteredRecipes);
 });
 
 app.listen(3001, () => {
