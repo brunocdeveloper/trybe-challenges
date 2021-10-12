af9123e320ad44add70248308562e4015f60d0ac
Bruno <brunocmoraes97@gmail.com>
2021-10-12 11:57:38 -0300

    Conexao com o banco de dados utilizando mysql

---
+++ AS-Camada-Model/index.js
@@ -0,0 +1,35 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const Author = require('./models/Author');
+
+app.use(bodyParser.json());
+
+app.get('/authors', async (req, res) => {
+  const authors = await Author.getAll();
+
+  res.status(200).json(authors);
+});
+
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+  const authors = await Author.findById(id);
+
+  if(!authors) return res.status(404).json({ message: 'not found' });
+
+  res.status(200).json(authors);
+});
+
+app.post('/authors', async (req, res) => {
+  const { firstName, middleName, lastName } = req.body;
+
+  if(!Author.isValid(firstName, middleName, lastName)) return res.status(404).json({ message: 'Dados inválidos' });
+
+  await Author.create(firstName, middleName, lastName);
+
+  res.status(201).json({ message: 'Author criado com sucesso '});
+});
+
+app.listen(port, () => console.log('Ouvindo na porta 3000'));
