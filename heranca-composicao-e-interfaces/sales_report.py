691b5608d8eefbdf50573643b23c1cf3c13532c5
Bruno <brunocmoraes97@gmail.com>
2022-01-31 11:00:10 -0300

    Refatoraçao com o conceito de herança

--- heranca-composicao-e-interfaces/sales_report.py
+++ heranca-composicao-e-interfaces/sales_report.py
@@ -1,14 +1,12 @@
+from abc import ABC, abstractmethod
 import json
 
 
-class SalesReport():
+class SalesReport(ABC):
     def __init__(self, export_file):
-        self.export_file = export_file + '.json'
+        self.export_file = export_file
 
     def build(self):
-        """ Aqui colocamos a lógica para a entidade 'se criar',
-        ou seja, criar um relatório imprimível. Por simplicidade,
-        vamos omitir essa lógica nos exemplos! """
         return [{
                 'Coluna 1': 'Dado 1',
                 'Coluna 2': 'Dado 2',
@@ -20,16 +18,12 @@ class SalesReport():
                 'Coluna 3': 'Dado C'
                 }]
 
+    @abstractmethod
     def serialize(self):
-        # Vamos gerar, aqui, o nosso relatório em formato JSON
-        with open(self.export_file, 'w') as file:
-            json.dump(self.build(), file)
+        raise NotImplementedError
 
-    def serialize_csv(self):
-        with open('meu_relatorio.csv', 'w') as file:
-            headers = ["Coluna 1", "Coluna 2", "Coluna 3"]
-            csv_writer = DictWriter(file, headers)
-            csv_writer.writeheader()
-            for item in self.build():
-                csv_writer.writerow(item)
-                  
+
+class SalesReportJSON(SalesReport):
+    def serialize(self):
+        with open(self.export_file + '.json', 'w') as file:
+            json.dump(self.build(), file)
