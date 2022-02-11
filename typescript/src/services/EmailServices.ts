7d4f34f432829b07c67f24acd945d20ec21da0dc
Bruno <brunocmoraes97@gmail.com>
2022-02-11 13:15:22 -0300

    node com ts

--- typescript/src/services/EmailServices.ts
+++ typescript/src/services/EmailServices.ts
@@ -9,8 +9,20 @@ interface IMailMessage {
   attachament?: string[];
 }
 
+// Data Transfer Object (DDD)
+interface IMessageDTO {
+  to: IMailTo,
+  message: IMailMessage,
+}
+
 class EmailService {
   sendMail(to: IMailTo, message: IMailMessage) {
+    console.log(`Email enviado para ${to.name}`);
+  }
+
+  // caso a função otherSendEmail seja usada no contoller,
+  // o parametro deverá ser nomeado to: { } e message: { }
+  otherSendEmail({ to, message }: IMessageDTO) {
     console.log(`Email enviado para ${to.name}`);
   }
 }
