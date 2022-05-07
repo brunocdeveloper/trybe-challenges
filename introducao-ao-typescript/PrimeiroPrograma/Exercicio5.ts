1201159fd025f00016510ca8495ac457b0cc94ec
Bruno <brunocmoraes97@gmail.com>
2022-05-07 16:02:17 -0300

    Exercicio 7. Cria funcao que calcula a area de um triangulo

--- introducao-ao-typescript/PrimeiroPrograma/Exercicio5.ts
+++ introducao-ao-typescript/PrimeiroPrograma/Exercicio5.ts
@@ -5,3 +5,15 @@ export function greeter(name: string) {
 export function personAge(name: string, age: number) {
   return `${name} tem ${age} anos!`;
 }
+
+export function add(x: number, y: number): number {
+  return x + y;
+}
+
+export function sumArray(numbers: number[]): number {
+  return numbers.reduce(add, 0);
+}
+
+export function triangle(base: number, height: number): number {
+  return (base * height) / 2;
+}
