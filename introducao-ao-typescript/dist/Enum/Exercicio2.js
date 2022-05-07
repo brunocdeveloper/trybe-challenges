f111c65cd601ddc4d1e496eaca96a9b23abb659d
Bruno <brunocmoraes97@gmail.com>
2022-05-07 16:10:54 -0300

    Compila os exercicios feitos em ts

---
+++ introducao-ao-typescript/dist/Enum/Exercicio2.js
@@ -0,0 +1,11 @@
+"use strict";
+var coresDoArcoIris;
+(function (coresDoArcoIris) {
+    coresDoArcoIris["colorOne"] = "violeta";
+    coresDoArcoIris["colorTwo"] = "anil";
+    coresDoArcoIris["colorThree"] = "azul";
+    coresDoArcoIris["coloFour"] = "verde";
+    coresDoArcoIris["colorFive"] = "amarelo";
+    coresDoArcoIris["colorSix"] = "laranja";
+    coresDoArcoIris["colorSeven"] = "vermelho";
+})(coresDoArcoIris || (coresDoArcoIris = {}));
